15adc43d9937c46569b620eb99f6d3ab1fa71615
kykong <ky_kong@yahoo.com>
2016-11-07 16:56:32 +0800

    Added repository code

---
+++ initial/companies/src/main/java/io/pivotal/demo/repositories/CompanyRepository.java
@@ -0,0 +1,9 @@
+package io.pivotal.demo.repositories;
+
+import io.pivotal.demo.domain.Company;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "companies", path = "companies")
+public interface CompanyRepository extends PagingAndSortingRepository<Company, Long> {
+}
